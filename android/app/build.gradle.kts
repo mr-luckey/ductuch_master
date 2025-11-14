c804f888f17c79bb45857af435a57c3eb3306695
mr-luckey <software0258@gmail.com>
2025-11-14 19:23:12 +0500

    final release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = maxOf(flutter.minSdkVersion, 24)
         targetSdk = 36
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        versionCode = 2  // Build number - must increment for each Play Store upload
+        versionName = "2.0.0"  // Version name shown to users
     }
 
     signingConfigs {
